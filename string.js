87717725d529dd2c63fe6edd9b85522c46aefabc
Rahat Karim <rahatkarim5452@gmail.com>
2026-07-21 20:35:47 +0600

    String.js File Added

---
+++ string.js
@@ -0,0 +1,15 @@
+const cup = "world cup";
+const tea = "Tea cup";
+const location = `Bandor bon`;
+const vehicle = new String("car");
+
+//Array Length Finding
+const numbers = [4, 5, 6, 7, 8, 8];
+console.log(numbers.length);
+console.log(numbers[0]);
+
+//For String Length Finding
+//*String Are immutable : you cannot change a string in place.
+console.log(location.length);
+console.log(location[0]);
+location[0] = "F"; //String k index diye Set Kora Jay na.
